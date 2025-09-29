f1687c5e66ba6956fc3b43b3c28be6f0d5e5912f
Camilo Arango <akami.3ds@gmail.com>
2025-09-29 16:31:28 -0500

    fix size

--- src/app/intro/page.jsx
+++ src/app/intro/page.jsx
@@ -10,7 +10,7 @@ export default function ARPage() {
         <a-marker preset="hiro">
           <a-entity 
                 gltf-model="/models/hornet/source/HORNET.glb" 
-                scale="0.5 0.5 0.5" 
+                scale="2.5" 
                 position="0 0 0" 
                 rotation="0 0 0">
             </a-entity>
