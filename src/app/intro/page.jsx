f3de1d7bb043c45dfebb7764181924c7d39be456
Camilo Arango <akami.3ds@gmail.com>
2025-09-29 17:25:42 -0500

    test gps

--- src/app/intro/page.jsx
+++ src/app/intro/page.jsx
@@ -1,23 +1,23 @@
 "use client"
 
-
 export default function Intro() {
-  
-
   return (
-    
-      <a-scene arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: true;" >
-        <a-marker preset="hiro">
-          <a-entity 
-                gltf-model="/models/hornet/source/HORNET.glb" 
-                scale="5 5 5" 
-                position="0 0 0" 
-                rotation="0 0 0">
-            </a-entity>
-        </a-marker>
+    <a-scene 
+      vr-mode-ui="enabled: false"
+      embedded
+      arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: true;"
+    >
 
-        <a-entity camera></a-entity>
-      </a-scene>
-    
-  );
+      {/* 📍 Modelo en coordenadas GPS */}
+      <a-entity
+        gltf-model="/models/hornet/source/HORNET.glb"
+        gps-entity-place="latitude: 3.89976041136871; longitude:  -76.30010948442047;"  
+        scale="5 5 5"
+        rotation="0 0 0"
+      ></a-entity>
+
+      {/* 🎥 Cámara con soporte GPS */}
+      <a-camera gps-camera rotation-reader></a-camera>
+    </a-scene>
+  )
 }
