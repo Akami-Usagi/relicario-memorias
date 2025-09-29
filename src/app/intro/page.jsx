9a14567fed386222662bfeac6f47f0224f63d16d
Camilo Arango <akami.3ds@gmail.com>
2025-09-29 14:56:49 -0500

    initial commit

---
+++ src/app/intro/page.jsx
@@ -0,0 +1,38 @@
+"use client"
+import { useEffect } from "react";
+
+export default function ARPage() {
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.src = "https://aframe.io/releases/1.5.0/aframe.min.js";
+    script1.async = true;
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.src = "https://cdn.jsdelivr.net/gh/AR-js-org/AR.js/aframe/build/aframe-ar.js";
+    script2.async = true;
+    document.body.appendChild(script2);
+
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    };
+  }, []);
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <a-scene
+        embedded
+        arjs="trackingMethod: best; sourceType: webcam;"
+        vr-mode-ui="enabled: false"
+        renderer="logarithmicDepthBuffer: true;"
+      >
+        <a-marker preset="hiro">
+          <a-box color="red" position="0 0.5 0"></a-box>
+        </a-marker>
+
+        <a-entity camera></a-entity>
+      </a-scene>
+    </div>
+  );
+}
