85b624bd85a97a96e45b0c09bc0cd4a4d92b3be0
Camilo Arango <akami.3ds@gmail.com>
2025-09-29 17:31:18 -0500

    test offset

--- src/app/intro/page.jsx
+++ src/app/intro/page.jsx
@@ -9,12 +9,13 @@ export default function Intro() {
     >
 
       {/* 📍 Modelo en coordenadas GPS */}
-      <a-entity
-        gltf-model="/models/hornet/source/HORNET.glb"
-        gps-entity-place="latitude: 3.89976041136871; longitude:  -76.30010948442047;"  
-        scale="50 50 50"
-        rotation="0 0 0"
-      ></a-entity>
+      <a-entity camera gps-camera rotation-reader>
+        <a-entity
+          gltf-model="/models/hornet/source/HORNET.glb"
+          position="0 0 -10"
+          scale="5 5 5"
+        ></a-entity>
+      </a-entity>
 
       {/* 🎥 Cámara con soporte GPS */}
       <a-camera gps-camera rotation-reader></a-camera>
