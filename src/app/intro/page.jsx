b7759351bdc9c888ef18bd86dffd019bcf663183
Camilo Arango <akami.3ds@gmail.com>
2025-09-30 20:47:22 -0500

    revert changes

--- src/app/intro/page.jsx
+++ src/app/intro/page.jsx
@@ -1,29 +1,34 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export default function Intro() {
+
   const [isClient, setIsClient] = useState(false);
+  
 
   useEffect(() => {
     setIsClient(true); // Marca que ya estamos en el cliente
-
     
   }, []);
 
   if (!isClient) return null; // No renderizar en SSR
 
   return (
-    <a-scene arjs xr-mode-ui="enabled: false" isMobile>
+    <a-scene 
+        arjs
+        xr-mode-ui="enabled: false"
+        isMobile
+        >
       <a-marker type="pattern" url="/markers/penta.patt">
-        <a-entity
-          position="0 0 0"
-          scale="5 5 5"
-          rotation="-90 0 0"
+        <a-entity 
+          position="0 0 0" 
+          scale="5 5 5" 
           gltf-model="/models/cat.glb"
-        ></a-entity>
+          rotation="-90 0 0"
+          ></a-entity>
       </a-marker>
-      <a-entity camera="fov: 60; near: 0.1; far: 10000" look-controls="enabled: false"></a-entity>
+      <a-entity camera="fov: 60; near: 0.1; far: 10000" 
+            look-controls="enabled: false"></a-entity>
     </a-scene>
   );
 }
-
