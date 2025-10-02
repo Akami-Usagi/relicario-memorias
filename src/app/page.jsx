c7ba4c7122c037c55510d2a87d8377b6145762e0
Camilo Arango <akami.3ds@gmail.com>
2025-10-02 13:18:22 -0500

    ajustes a video y doble idioma

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <a href="comedor/spanish">ESPAÑOL</a>
+      <a href="comedor/english">ENGLISH</a>
+    </div>
+  );
+}
