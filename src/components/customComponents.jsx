918cf53c7d16a5e55c3b6af2925556dae271330c
Camilo Arango <akami.3ds@gmail.com>
2025-10-02 15:58:56 -0500

    separacion de styled components

---
+++ src/components/customComponents.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components"
+
+export const MainDiv = styled.div`
+    width: 100%;
+    height: 100dvh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+` 
+
+export const LogoImage = styled.img`
+    width: 300px;
+`
+export const DefaultButton = styled.button`
+    width: 150px;
+    height: 80px;
+    background-color: #4ea6ce;
+    border-radius: 15px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: x-large;
+    color: white;
+    border: none;
+    box-shadow: 5px 5px 3px rgba(0,0,0,0.2);
+    
+`
+export const AreaTitle = styled.h1`
+    font-family: Arial, Helvetica, sans-serif;
+    margin-top: -50px;
+`
+export const AreaText = styled.p`
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: medium;
+    margin-top: -30px;
+    margin-bottom: 50px;
+`
